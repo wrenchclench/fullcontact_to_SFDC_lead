6bd34f6fdf6de8444e2a2672106ba4a077d9d898
John Budreski <john.budreski@gmail.com>
2018-04-09 12:05:26 +0800

    Salesforce REST API integrated

--- main/java/fullContactInsights/Application.java
+++ main/java/fullContactInsights/Application.java
@@ -6,15 +6,16 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-public class Application {
+//@SpringBootApplication
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+//}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-}
